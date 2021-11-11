a4243cf231c5c9cb8d8abe997b7165ebeea5f8da
Benji Altman <benji@altmansoftwaredesign.com>
2021-11-11 09:50:53 -0600

    comment datareaderfilter

--- SpecialDataReaders/DataReaderFilter.cs
+++ SpecialDataReaders/DataReaderFilter.cs
@@ -4,159 +4,123 @@ using System.Data;
 namespace Gerk.SpecialDataReaders
 {
 	//Data reader to let you filter columns out a data reader. May want to rethink how this is designed to bring it more inline with other implementations in this file.
+	/// <summary>
+	/// Wraps another datareader filtering out specific rows based on a predicate.
+	/// </summary>
+	/// <typeparam name="T">The underlying datareader type</typeparam>
 	public class DataReaderFilter<T> : IDataReader where T : IDataReader
 	{
-		T me;
-		Predicate<T> include;
+		readonly T me;
+		readonly Predicate<T> include;
 
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="dataReader">The underlying datareader.</param>
+		/// <param name="where">Predicate that filters rows. Only keep the rows where this returns true. The argument will be the datareader once it has progressed to the current point.</param>
 		public DataReaderFilter(T dataReader, Predicate<T> where)
 		{
 			me = dataReader;
 			include = where;
 		}
 
+		/// <inheritdoc/>
 		public object this[int i] => me[i];
 
+		/// <inheritdoc/>
 		public object this[string name] => me[name];
 
+		/// <inheritdoc/>
 		public int Depth => me.Depth;
 
+		/// <inheritdoc/>
 		public bool IsClosed => me.IsClosed;
 
+		/// <inheritdoc/>
 		public int RecordsAffected => me.RecordsAffected;
 
+		/// <inheritdoc/>
 		public int FieldCount => me.FieldCount;
 
-		public void Close()
-		{
-			me.Close();
-		}
+		/// <inheritdoc/>
+		public void Close() => me.Close();
 
-		public void Dispose()
-		{
-			me.Dispose();
-		}
+		/// <inheritdoc/>
+		public void Dispose() => me.Dispose();
 
-		public bool GetBoolean(int i)
-		{
-			return me.GetBoolean(i);
-		}
+		/// <inheritdoc/>
+		public bool GetBoolean(int i) => me.GetBoolean(i);
 
-		public byte GetByte(int i)
-		{
-			return me.GetByte(i);
-		}
+		/// <inheritdoc/>
+		public byte GetByte(int i) => me.GetByte(i);
 
-		public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length)
-		{
-			return me.GetBytes(i, fieldOffset, buffer, bufferoffset, length);
-		}
+		/// <inheritdoc/>
+		public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length) => me.GetBytes(i, fieldOffset, buffer, bufferoffset, length);
 
-		public char GetChar(int i)
-		{
-			return me.GetChar(i);
-		}
+		/// <inheritdoc/>
+		public char GetChar(int i) => me.GetChar(i);
 
-		public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length)
-		{
-			return me.GetChars(i, fieldoffset, buffer, bufferoffset, length);
-		}
+		/// <inheritdoc/>
+		public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length) => me.GetChars(i, fieldoffset, buffer, bufferoffset, length);
 
-		public IDataReader GetData(int i)
-		{
-			return me.GetData(i);
-		}
+		/// <inheritdoc/>
+		public IDataReader GetData(int i) => me.GetData(i);
 
-		public string GetDataTypeName(int i)
-		{
-			return me.GetDataTypeName(i);
-		}
+		/// <inheritdoc/>
+		public string GetDataTypeName(int i) => me.GetDataTypeName(i);
 
-		public DateTime GetDateTime(int i)
-		{
-			return me.GetDateTime(i);
-		}
+		/// <inheritdoc/>
+		public DateTime GetDateTime(int i) => me.GetDateTime(i);
 
-		public decimal GetDecimal(int i)
-		{
-			return me.GetDecimal(i);
-		}
+		/// <inheritdoc/>
+		public decimal GetDecimal(int i) => me.GetDecimal(i);
 
-		public double GetDouble(int i)
-		{
-			return me.GetDouble(i);
-		}
+		/// <inheritdoc/>
+		public double GetDouble(int i) => me.GetDouble(i);
 
-		public Type GetFieldType(int i)
-		{
-			return me.GetFieldType(i);
-		}
+		/// <inheritdoc/>
+		public Type GetFieldType(int i) => me.GetFieldType(i);
 
-		public float GetFloat(int i)
-		{
-			return me.GetFloat(i);
-		}
+		/// <inheritdoc/>
+		public float GetFloat(int i) => me.GetFloat(i);
 
-		public Guid GetGuid(int i)
-		{
-			return me.GetGuid(i);
-		}
+		/// <inheritdoc/>
+		public Guid GetGuid(int i) => me.GetGuid(i);
 
-		public short GetInt16(int i)
-		{
-			return me.GetInt16(i);
-		}
+		/// <inheritdoc/>
+		public short GetInt16(int i) => me.GetInt16(i);
 
-		public int GetInt32(int i)
-		{
-			return me.GetInt32(i);
-		}
+		/// <inheritdoc/>
+		public int GetInt32(int i) => me.GetInt32(i);
 
-		public long GetInt64(int i)
-		{
-			return me.GetInt64(i);
-		}
+		/// <inheritdoc/>
+		public long GetInt64(int i) => me.GetInt64(i);
 
-		public string GetName(int i)
-		{
-			return me.GetName(i);
-		}
+		/// <inheritdoc/>
+		public string GetName(int i) => me.GetName(i);
 
-		public int GetOrdinal(string name)
-		{
-			return me.GetOrdinal(name);
-		}
+		/// <inheritdoc/>
+		public int GetOrdinal(string name) => me.GetOrdinal(name);
 
-		public DataTable GetSchemaTable()
-		{
-			return me.GetSchemaTable();
-		}
+		/// <inheritdoc/>
+		public DataTable GetSchemaTable() => me.GetSchemaTable();
 
-		public string GetString(int i)
-		{
-			return me.GetString(i);
-		}
+		/// <inheritdoc/>
+		public string GetString(int i) => me.GetString(i);
 
-		public object GetValue(int i)
-		{
-			return me.GetValue(i);
-		}
+		/// <inheritdoc/>
+		public object GetValue(int i) => me.GetValue(i);
 
-		public int GetValues(object[] values)
-		{
-			return me.GetValues(values);
-		}
+		/// <inheritdoc/>
+		public int GetValues(object[] values) => me.GetValues(values);
 
-		public bool IsDBNull(int i)
-		{
-			return me.IsDBNull(i);
-		}
+		/// <inheritdoc/>
+		public bool IsDBNull(int i) => me.IsDBNull(i);
 
-		public bool NextResult()
-		{
-			return me.NextResult();
-		}
+		/// <inheritdoc/>
+		public bool NextResult() => me.NextResult();
 
+		/// <inheritdoc/>
 		public bool Read()
 		{
 			//I know this is hard to read, but I couldn't think of a better way to do this.
